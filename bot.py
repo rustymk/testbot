a3a409ae6319ed20c1b769995306b96dd6ec9fc8
rustymk <rusty.mk@gmail.com>
2018-03-19 15:11:00 +0300

    First commit

---
+++ bot.py
@@ -0,0 +1,23 @@
+import telebot
+import parser
+
+#main variables
+TOKEN = ""
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду спикером ОСК =)')
+
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if text == "привет":
+        bot.send_message(chat_id, 'Привет, я бот-страйкболист.')
+    elif text == "как дела?":
+        bot.send_message(chat_id, 'Хорошо, а у тебя?')
+    else:
+        bot.send_message(chat_id, 'Простите, я вас не понял :(')
+        
+bot.polling(none_stop=True)
